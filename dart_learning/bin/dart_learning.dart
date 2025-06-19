e8f6d918f2fcfdbb048070b4ed014afd61bdf2c9
Matvii Ulytskyi <matveyulitskiy@gmail.com>
2025-06-19 06:32:18 +0200

    feat(DART-06): added math operations task

--- dart_learning/bin/dart_learning.dart
+++ dart_learning/bin/dart_learning.dart
@@ -1,3 +1,20 @@
 void main(){
-
+  String separator = List.filled(40, '=').join();
+  print(separator);
+  print("Task 1");
+  print(separator);
+  mathOperations();
+  print(separator);
 }
+
+void mathOperations(){
+  double width = 18.4;
+  double height = 30.9;
+  double area = width * height;
+  double perimeter = (width + height) * 2;
+  int squares = area ~/ 1;
+  print("Area is $area");
+  print("Perimeter is $perimeter");
+  print("Full 1x1 squares: $squares");
+}
+
