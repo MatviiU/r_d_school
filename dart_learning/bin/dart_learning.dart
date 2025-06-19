ba5c328156217349b156571aabb9d7834886531d
Matvii Ulytskyi <matveyulitskiy@gmail.com>
2025-06-19 06:33:50 +0200

    feat(DART-06): added comparison operations task

--- dart_learning/bin/dart_learning.dart
+++ dart_learning/bin/dart_learning.dart
@@ -1,9 +1,14 @@
+import 'dart:io';
+
 void main(){
   String separator = List.filled(40, '=').join();
   print(separator);
   print("Task 1");
   print(separator);
   mathOperations();
+  print(separator);
+  print("Task 2");
+  comparisonOperations();
   print(separator);
 }
 
@@ -18,3 +23,17 @@ void mathOperations(){
   print("Full 1x1 squares: $squares");
 }
 
+void comparisonOperations(){
+  late double number;
+  stdout.write("Enter a number: ");
+  String? inputNumber = stdin.readLineSync();
+  number = double.parse(inputNumber!);
+
+  bool isGreaterThan50 = number > 50;
+  bool isLessThan100 = number < 100;
+  bool isDivisibleBy5 = number % 5 == 0;
+
+  print('Is the number greater than 50? $isGreaterThan50');
+  print('Is the number less than 100? $isLessThan100');
+  print('Is the number divisible by 5? $isDivisibleBy5');
+}
