b028d4ce067fa94e976e29b145004d6972f1a033
Matvii Ulytskyi <matveyulitskiy@gmail.com>
2025-06-19 06:58:56 +0200

    feat(DART-06): added ternary operations task

--- dart_learning/bin/dart_learning.dart
+++ dart_learning/bin/dart_learning.dart
@@ -2,51 +2,79 @@ import 'dart:io';
 
 void main(){
   String separator = List.filled(40, '=').join();
+
+  // print(separator);
+  // print("Task 1");
+  // print(separator);
+  // mathOperations();
+  //
+  // print(separator);
+  // print("Task 2");
+  // print(separator);
+  // comparisonOperations();
+  //
+  // print(separator);
+  // print("Task 3");
+  // print(separator);
+  // logicalOperations();
+
   print(separator);
-  print("Task 1");
+  print("Task 4");
   print(separator);
-  mathOperations();
-  print(separator);
-  print("Task 2");
-  comparisonOperations();
-  print(separator);
-  print("Task 3");
-  logicalOperations();
+  ternaryOperator();
   print(separator);
 }
 
-void mathOperations(){
-  double width = 18.4;
-  double height = 30.9;
-  double area = width * height;
-  double perimeter = (width + height) * 2;
-  int squares = area ~/ 1;
-  print("Area is $area");
-  print("Perimeter is $perimeter");
-  print("Full 1x1 squares: $squares");
-}
+// void mathOperations(){
+//   double width = 18.4;
+//   double height = 30.9;
+//   double area = width * height;
+//   double perimeter = (width + height) * 2;
+//   int squares = area ~/ 1;
+//   print("Area is $area");
+//   print("Perimeter is $perimeter");
+//   print("Full 1x1 squares: $squares");
+// }
+//
+// void comparisonOperations(){
+//   late double number;
+//   stdout.write("Enter a number: ");
+//   String? inputNumber = stdin.readLineSync();
+//   number = double.parse(inputNumber!);
+//
+//   bool isGreaterThan50 = number > 50;
+//   bool isLessThan100 = number < 100;
+//   bool isDivisibleBy5 = number % 5 == 0;
+//
+//   print('Is the number greater than 50? $isGreaterThan50');
+//   print('Is the number less than 100? $isLessThan100');
+//   print('Is the number divisible by 5? $isDivisibleBy5');
+// }
+//
+// void logicalOperations(){
+//   bool hasMoney = true;
+//   bool isStoreOpen = true;
+//
+//   bool canBuy = hasMoney && isStoreOpen;
+//   bool mustWait = !hasMoney || !isStoreOpen;
+//   print('Can buy: $canBuy');
+//   print('Must wait: $mustWait');
+// }
 
-void comparisonOperations(){
-  late double number;
-  stdout.write("Enter a number: ");
-  String? inputNumber = stdin.readLineSync();
-  number = double.parse(inputNumber!);
+void  ternaryOperator(){
+  late double temperature;
+  stdout.write("Enter a temperature: ");
+  String? inputTemperature = stdin.readLineSync();
+  temperature = double.parse(inputTemperature!);
+  bool isHot = temperature > 25;
+  bool isCold = temperature < 10;
 
-  bool isGreaterThan50 = number > 50;
-  bool isLessThan100 = number < 100;
-  bool isDivisibleBy5 = number % 5 == 0;
+  String message1 = isHot ? "Тепло" : "Прохолодно";
+  String message2 = isCold ? "Дуже холодно" : "";
 
-  print('Is the number greater than 50? $isGreaterThan50');
-  print('Is the number less than 100? $isLessThan100');
-  print('Is the number divisible by 5? $isDivisibleBy5');
-}
-
-void logicalOperations(){
-  bool hasMoney = true;
-  bool isStoreOpen = true;
-
-  bool canBuy = hasMoney && isStoreOpen;
-  bool mustWait = !hasMoney || !isStoreOpen;
-  print('Can buy: $canBuy');
-  print('Must wait: $mustWait');
+  if(message2.isNotEmpty){
+    print("The weather is: $message2");
+  } else {
+    print("The weather is: $message1");
+  }
 }
